6107ebe58b8e6d81c506c60b08a3a8e726535a88
DarthDandelion <darthdandelion@galaxy.milkyway.earth>
2013-08-03 12:06:07 -0700

    adding net code

--- engine/qcommon/qcommon.h
+++ engine/qcommon/qcommon.h
@@ -618,7 +618,7 @@ extern qboolean userinfo_modified;
 
 // net.h -- quake's interface to the networking layer
 
-#define PORT_ANY    -1
+#define PORT_ANY -1
 
 //Knightmare- increase max message size to eliminate SZ_Getspace: Overflow
 //Mark Shan is just gonna love this!!
@@ -633,20 +633,23 @@ extern qboolean userinfo_modified;
 
 typedef enum
 {
-    NA_LOOPBACK, NA_BROADCAST, NA_IP, NA_IPX, NA_BROADCAST_IPX
+    NA_LOOPBACK,
+    NA_BROADCAST,
+    NA_IP,
+    NA_IPX,
+    NA_BROADCAST_IPX,
+    NA_IP6,
+    NA_MULTICAST6
 } netadrtype_t;
 
-typedef enum
-{
-    NS_CLIENT, NS_SERVER
-} netsrc_t;
+typedef enum {NS_CLIENT, NS_SERVER} netsrc_t;
 
 typedef struct
 {
-    netadrtype_t   type;
-
-    byte           ip[4];
-    byte           ipx[10];
+    netadrtype_t type;
+    byte ip[16];
+    unsigned int scope_id;
+    byte ipx[10];
 
     unsigned short port;
 } netadr_t;
@@ -656,7 +659,8 @@ void NET_Shutdown(void);
 
 void NET_Config(qboolean multiplayer);
 
-qboolean NET_GetPacket(netsrc_t sock, netadr_t *net_from, sizebuf_t *net_message);
+qboolean NET_GetPacket(netsrc_t sock, netadr_t *net_from,
+        sizebuf_t *net_message);
 void NET_SendPacket(netsrc_t sock, int length, void *data, netadr_t to);
 
 qboolean NET_CompareAdr(netadr_t a, netadr_t b);
@@ -666,50 +670,48 @@ char *NET_AdrToString(netadr_t a);
 qboolean NET_StringToAdr(char *s, netadr_t *a);
 void NET_Sleep(int msec);
 
-//============================================================================
+/*=================================================================== */
 
-#define OLD_AVG       0.99              // total = oldtotal*OLD_AVG + new*(1-OLD_AVG)
-
-#define MAX_LATENT    32
+#define OLD_AVG 0.99
+#define MAX_LATENT 32
 
 typedef struct
 {
-    qboolean  fatal_error;
+    qboolean fatal_error;
 
-    netsrc_t  sock;
+    netsrc_t sock;
 
-    int       dropped;                                  // between last packet and previous
+    int dropped;                    /* between last packet and previous */
 
-    int       last_received;                            // for timeouts
-    int       last_sent;                                // for retransmits
+    int last_received;              /* for timeouts */
+    int last_sent;                  /* for retransmits */
 
-    netadr_t  remote_address;
-    int       qport;                                            // qport value to write when transmitting
+    netadr_t remote_address;
+    int qport;                      /* qport value to write when transmitting */
 
-// sequencing variables
-    int       incoming_sequence;
-    int       incoming_acknowledged;
-    int       incoming_reliable_acknowledged;                   // single bit
+    /* sequencing variables */
+    int incoming_sequence;
+    int incoming_acknowledged;
+    int incoming_reliable_acknowledged;         /* single bit */
 
-    int       incoming_reliable_sequence;                       // single bit, maintained local
+    int incoming_reliable_sequence;             /* single bit, maintained local */
 
-    int       outgoing_sequence;
-    int       reliable_sequence;                                // single bit
-    int       last_reliable_sequence;                           // sequence number of last send
+    int outgoing_sequence;
+    int reliable_sequence;                  /* single bit */
+    int last_reliable_sequence;             /* sequence number of last send */
 
-// reliable staging and holding areas
-    sizebuf_t message;                          // writing buffer to send to server
-    byte      message_buf[MAX_MSGLEN - 16];     // leave space for header
+    /* reliable staging and holding areas */
+    sizebuf_t message;          /* writing buffer to send to server */
+    byte message_buf[MAX_MSGLEN - 16];          /* leave space for header */
 
-// message is copied to this buffer when it is first transfered
-    int       reliable_length;
-    byte      reliable_buf[MAX_MSGLEN - 16];            // unacked reliable message
+    /* message is copied to this buffer when it is first transfered */
+    int reliable_length;
+    byte reliable_buf[MAX_MSGLEN - 16];         /* unacked reliable message */
 } netchan_t;
 
-extern netadr_t  net_from;
+extern netadr_t net_from;
 extern sizebuf_t net_message;
-extern byte      net_message_buffer[MAX_MSGLEN];
-
+extern byte net_message_buffer[MAX_MSGLEN];
 
 void Netchan_Init(void);
 void Netchan_Setup(netsrc_t sock, netchan_t *chan, netadr_t adr, int qport);
@@ -721,7 +723,6 @@ void Netchan_OutOfBandPrint(int net_socket, netadr_t adr, char *format, ...);
 qboolean Netchan_Process(netchan_t *chan, sizebuf_t *msg);
 
 qboolean Netchan_CanReliable(netchan_t *chan);
-
 
 /*
  * ==============================================================
